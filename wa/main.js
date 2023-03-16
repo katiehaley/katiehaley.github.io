a31c7a4f605b0ee744e105d89a8f9e9fee1a94b6
katiehaley <kaha1011@colorado.edu>
2023-03-16 11:55:32 -0600

    update

---
+++ wa/main.js
@@ -0,0 +1,54 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let insertY = ["the soup kitchen", "Disneyland","the White House"];
+let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+}
+
+function result() {
+
+    let newStory = storyText
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replace (":insertx:", xItem).replace("inserty",xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
+
+  if(customName.value !== '') {
+    let name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300*0.07).toString() + ' stone';
+    const temperature =  Math.round((94-32)/1.8).toString() + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit',temperature).replace('300 pounds',weight);
+
+
+   //let stoneweight = weight/14;
+    //let celtemp = (temperature-32)*.5556;
+
+    //let stonestring = stoneweight.toString() + "stone";
+    //let celtstring = celtemp.toString() + "celsius";
+
+    //newStory = newStory.replace("fahrenheit", celtemp).replace("pounds", stoneweight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
+
+randomize.addEventListener('click', result);
