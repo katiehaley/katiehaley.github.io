d74e1be8b1403bcb880fd79eb772dd86cb4db3c3
katiehaley <kaha1011@colorado.edu>
2023-04-04 10:37:51 -0600

    update!

---
+++ ica/ica11.js
@@ -0,0 +1,29 @@
+const button = document.querySelector("#js-new-quote");
+button.addEventListener('click', getQuote);
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function getQuote() {
+    //console.log("It works!");
+    try{
+        const response = await fetch (endpoint)
+        if (!response.ok){
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+        //console.log(json.question);
+        displayQuote(json.question);
+        //console.log(json.answer);
+    }
+    catch (err) {
+        console.log(err);
+        alert('Failed to fetch new trivias');
+    }
+}
+
+function displayQuote(quote){
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
+}
+
+getQuote();
